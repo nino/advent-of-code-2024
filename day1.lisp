b94ae8711c8e271c5ad4b7a63b826b5050ff38bf
Nino Annighoefer <nino@ninoan.com>
2024-12-01 20:16:14 +0100

    Tidy day 1

--- day1.lisp
+++ day1.lisp
@@ -7,8 +7,7 @@
 
 (in-package :day1)
 
-; Part 1
-(defun part1 ()
+(defun prep (input-file)
   (let* ((lines (uiop:read-file-lines "day1-input.txt"))
          (pairs (mapcar #'str:words lines))
          (left (mapcar (lambda (pair)
@@ -17,21 +16,20 @@
          (right (mapcar (lambda (pair)
                           (parse-integer (second pair)))
                         pairs)))
+    (list left right)))
+
+; Part 1
+(defun part1 ()
+  (let* ((prepped (prep "day1-input.txt")))
     (loop for i in (mapcar (lambda (l r) (abs (- l r)))
-                           (sort left #'<)
-                           (sort right #'<))
+                           (sort (first prepped) #'<)
+                           (sort (second prepped) #'<))
           summing i)))
 (part1)
 
 ; Part 2
 (defun part2 ()
-  (let* ((lines (uiop:read-file-lines "day1-input.txt"))
-         (pairs (mapcar #'str:words lines))
-         (left (mapcar (lambda (pair)
-                         (parse-integer (first pair)))
-                       pairs))
-         (right (mapcar (lambda (pair)
-                          (parse-integer (second pair)))
-                        pairs)))
-    (loop for l in left summing (* l (count l right)))))
+  (let* ((prepped (prep "day1-input.txt")))
+    (loop for l in (first prepped)
+          summing (* l (count l (second prepped))))))
 (part2)
