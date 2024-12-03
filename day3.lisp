aa9cbaf3d0ad9869192c78e27d2fa7cd52a8af65
Nino Annighoefer <nino@ninoan.com>
2024-12-03 13:22:08 +0100

    Day 3 part 2

--- day3.lisp
+++ day3.lisp
@@ -6,8 +6,6 @@
 
 (in-package :day3)
 
-
-
 (defun calc-ops (ops)
   (loop for pair in ops
         sum (* (first pair) (second pair))))
@@ -23,3 +21,19 @@
 (part1 (uiop:read-file-string "day3-input.txt"))
 
 (part1 "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+
+(defun part2 (input)
+  (let ((ops (ppcre:all-matches-as-strings
+               "don't\\(\\)|do\\(\\)|mul\\((\\d+),(\\d+)\\)"
+               input))
+        (enabled t))
+    (calc-ops (loop for op-str in ops
+                    if (equal op-str "do()")
+                    do (setf enabled t)
+                    else if (equal op-str "don't()")
+                    do (setf enabled nil)
+                    else if enabled
+                    collect (parse-op op-str)))))
+
+(part2 (uiop:read-file-string "day3-input.txt"))
+(part2 "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
